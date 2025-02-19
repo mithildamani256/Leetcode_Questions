2889adf1f8e7c08d14c7fd0552f56bc2be0a168a
mithildamani256 <63705029+mithildamani256@users.noreply.github.com>
2025-02-19 15:13:12 -0500

    Time: 47 ms (76.23%) | Memory: 19.5 MB (11.42%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -4,59 +4,19 @@ class Solution(object):
         :type prices: List[int]
         :rtype: int
         """
-        
-        # # max_diff = 0
-
-        # # for i in range(len(prices)):
-        # #     val = prices[i]
-        # #     for j in range(i+1, len(prices)):
-        # #         stck = prices[j]
-        # #         if (stck - val > max_diff):
-        # #             max_diff = stck-val
-
-        
-        # # return max_diff
-
-        # max_profit = 0
-
-        # buy = prices[0]
-
-        # for stock in prices[1:]:
-        #     if stock > buy:
-        #         max_profit = max(stock-buy, max_profit)
-        #     else:
-        #         buy = stock
-        
-        # return max_profit
-
-        # n = len(prices)
-
-        # max_value = 0
-
-        # for i in range(n):
-        #     for j in range(i+1,n):
-        #         if prices[j] - prices[i] > max_value:
-        #             max_value = prices[j] - prices[i]
-
-
-        # return max_value
-
-#         The idea of Kadane’s algorithm is to traverse over the array from left to right and for each element,
-#  find the maximum sum ending at that element. The result will be the maximum of all these values. 
 
         max_profit = 0
-        min_value = prices[0]
+        buy_day = 0
 
-        for price in prices:
-            if price - min_value > max_profit:
-                max_profit = price - min_value
+        for i in range(1, len(prices)):
+            if prices[i] - prices[buy_day] > max_profit:
+                max_profit = prices[i] - prices[buy_day]
+            
+            if prices[i] < prices[buy_day]:
+                buy_day = i
 
-            if price < min_value:
-                min_value = price
-
-        
         return max_profit
 
 
 
-
+        
