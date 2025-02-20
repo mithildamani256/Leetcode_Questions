c06fbe6eccbd46d8278227e6be9f208a2e17bf10
mithildamani256 <63705029+mithildamani256@users.noreply.github.com>
2025-02-20 16:43:58 -0500

    Time: 39 ms (39.94%) | Memory: 12.8 MB (14.85%) - LeetSync

--- 383-ransom-note/ransom-note.py
+++ 383-ransom-note/ransom-note.py
@@ -1,36 +1,20 @@
 class Solution(object):
     def canConstruct(self, ransomNote, magazine):
-
         """
         :type ransomNote: str
         :type magazine: str
         :rtype: bool
         """
 
-        # for i in ransomNote:
-        #     if i in magazine:
-        #         val = magazine.index(i)
-        #         magazine = magazine[:val] + magazine[val+1:]
-        #     else:
-        #         return False
+        values = {}
 
-        # return True            
-        usable = {}
+        for val in magazine:
+            values[val] = values.get(val, 0) + 1
 
-        for s in magazine:
-            if (s in usable):
-                usable[s] += 1
-            else:
-                usable[s] = 1
-        
-        for s in ransomNote:
-            if (s not in usable):
+        for val in ransomNote:
+            if values.get(val, 0) == 0:
                 return False
-            if (s in usable):
-                if (usable[s] == 0):
-                    return False
-                else:
-                    usable[s] -= 1
-        
-
+            values[val] -= 1
+    
         return True
+        
