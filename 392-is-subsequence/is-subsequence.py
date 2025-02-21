3f63f558e7a60f3ecc463c39f1762b936cc37b90
mithildamani256 <63705029+mithildamani256@users.noreply.github.com>
2025-02-20 20:05:06 -0500

    Time: 0 ms (100.00%) | Memory: 12.4 MB (94.78%) - LeetSync

--- 392-is-subsequence/is-subsequence.py
+++ 392-is-subsequence/is-subsequence.py
@@ -5,47 +5,21 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
-        # i = 0
-        # j = 0
-
-        # if(s == ""):
-        #     return True
         
-        # while(j < len(t) and i < len(s)):
-        #     if(s[i] == t[j]):
-        #         i += 1
-        #     if(i == len(s)):
-        #         return True          
-        #     j += 1
-        
-        # return False
+        l = 0
+        r = 0
 
-        # current = -1
+        while r < len(t):
+            if l >= len(s):
+                return True
+                
+            if s[l] == t[r]:
+                l += 1
+                r += 1
+            else:
+                r += 1
 
-        # for i in range(len(s)):
-        #     index = t.find(s[i])
-        #     if index > current:
-        #         current = index
-        #     else:
-        #         return False
-        
-        # return True
+        if l == len(s):
+            return True
 
-        x = -1
-        flag = False
-
-        for i in range(len(s)):
-            flag = False
-            val = s[i]
-
-            for j in range(x + 1, len(t)):
-                if t[j] == val:
-                    x = j
-                    flag = True
-                    break
-            
-            if flag == False:
-                return False
-        
-        return True
-            
+        return False
