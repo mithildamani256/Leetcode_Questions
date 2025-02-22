a86ff58aa94c4471d8317d037be911743d3c181f
mithildamani256 <63705029+mithildamani256@users.noreply.github.com>
2025-02-22 11:03:44 -0500

    Time: 27 ms (47.20%) | Memory: 13.3 MB (15.81%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -1,54 +1,22 @@
 class Solution(object):
     def lengthOfLongestSubstring(self, s):
-        # maxlen_substr = 0
-        # max_substr = ""
-        # substr = ""
+        """
+        :type s: str
+        :rtype: int
+        """
 
-        # for x in s:
-        #     if x not in substr:
-        #         substr += x
-        #     else:
-        #         if (len(substr) > maxlen_substr):
-        #             maxlen_substr = len(substr)
-        #             max_substr = substr
-        #             substr = x
-        
-        # # print(max_substr)
-        # return maxlen_substr
+        max_length = 0
+        substr = ""
 
-        # l = 0
-        # x = ""
-        # max_len = 0
-        
-        # for r in range(len(s)):
-        #     if(s[r] in x):
-        #         max_len = max(len(x), max_len)
-        #         l += 1
-        #         x = s[l:r]
-
-        #     else:
-        #         x += s[r]
-
-        # return max_len
-
-        if s == "":
-            return 0
-
-        longest_length = 1
-        substring = s[0]
-        longest_substring = s[0]
-
-        start = 0
-
-        for end in range(1, len(s)):
-            if s[end] not in substring:
-                substring = substring + s[end]
+        for right in range(len(s)):
+            if s[right] not in substr:
+                substr = substr + s[right]
+                max_length = max(max_length, len(substr))
             else:
-                start = s.find(s[end], start) + 1
-                substring = s[start : end + 1]
-            
-            if (len(substring) > longest_length):
-                longest_length = len(substring)
-                longest_substring = substring
+                while s[right] in substr:
+                    substr = substr[1:]
+                substr += s[right]
+
+
+        return max_length
         
-        return longest_length
