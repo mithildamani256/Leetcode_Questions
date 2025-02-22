a7d666a52f5cce542738b7ef6b2a40ad3d0e31e2
mithildamani256 <63705029+mithildamani256@users.noreply.github.com>
2025-02-22 09:31:34 -0500

    Time: 3 ms (65.40%) | Memory: 23.1 MB (24.21%) - LeetSync

--- 189-rotate-array/rotate-array.py
+++ 189-rotate-array/rotate-array.py
@@ -8,15 +8,24 @@ class Solution(object):
 
         k = k % len(nums)
 
-        nums.reverse()
+        # nums.reverse()
 
-        temp = nums[:k]
-        temp.reverse()
-        nums[:k] = temp
+        # temp = nums[:k]
+        # temp.reverse()
+        # nums[:k] = temp
 
-        temp = nums[k:]
-        temp.reverse()
-        nums[k:] = temp
+        # temp = nums[k:]
+        # temp.reverse()
+        # nums[k:] = temp
+        l = k
+        k = len(nums) - k
 
+        temp = nums[ :k]
+        temp2 = nums[k:]
+
+        nums[:l] = temp2
+        nums[l:] = temp
+
+        return nums
 
 
