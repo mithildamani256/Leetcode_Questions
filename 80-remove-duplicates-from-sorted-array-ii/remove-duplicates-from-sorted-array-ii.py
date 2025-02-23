8df8fee9d7a6169e23ea130e791eea6736a72e8a
mithildamani256 <63705029+mithildamani256@users.noreply.github.com>
2025-02-22 19:10:00 -0500

    Time: 72 ms (21.12%) | Memory: 15.4 MB (21.61%) - LeetSync

--- 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.py
+++ 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.py
@@ -5,39 +5,26 @@ class Solution(object):
         :rtype: int
         """
 
-        # index = 1
-        # count = 1
-        # val = nums[0]
+        if len(nums) <= 2:
+            return len(nums) 
 
-        # for i in range(1,len(nums)):
-        #     if(val == nums[i]):
-        #         count += 1
+        hashmap = {}
 
-        #         if(count <= 2):
-        #             nums[index] = val
-        #             index += 1
-        #     else:
-        #         nums[index] = nums[i]
-        #         count = 1
-        #         val = nums[index]
-        #         index += 1
+        for i in range(2):
+            hashmap[nums[i]] = hashmap.get(nums[i], 0) + 1
 
+        k = 2
         
-        # return index 
+        for i in range(2, len(nums)):
+            value = hashmap.get(nums[i], 0)
 
-        count = 1
-        replace = 1
+            if value <= 1:
+                nums[k] = nums[i]
+                k += 1
+            
+            hashmap[nums[i]] = hashmap.get(nums[i], 0) + 1
 
-        for i in range(1, len(nums)):
-            if nums[i-1] != nums[i]:
-                nums[replace] = nums[i]
-                count = 1
-                replace += 1
-            else:
-                count += 1
-                if count <= 2:
-                    nums[replace] = nums[i]
-                    replace += 1
 
-        return replace 
+        return k
+
         
