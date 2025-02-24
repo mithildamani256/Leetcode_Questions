80170fe2e756985ad4c0c7b19126aa1dea6f1b1d
mithildamani256 <63705029+mithildamani256@users.noreply.github.com>
2025-02-24 10:28:46 -0500

    Time: 0 ms (100.00%) | Memory: 12.4 MB (91.45%) - LeetSync

--- 129-sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.py
+++ 129-sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.py
@@ -9,48 +9,20 @@ class Solution(object):
         """
         :type root: Optional[TreeNode]
         :rtype: int
+
         """
 
-        # def dfs(root):
-        #     if not root:
-        #         return ""
+        def helper(root, current_sum):
 
-        #     value = str(root.val)
-        #     result = [value]
-
-        #     for s in dfs(root.left):
-        #         result.append(value + s)
-
-        #     for s in dfs(root.right):
-        #         result.append(value + s)
-
-        #     return result
-
-        # result1 = dfs(root.left)
-        # result2 = dfs(root.right)
-        # sum = 0
-
-        # for val in result1:
-        #     sum += int(str(root.val) + val)
-        # for val in result2:
-        #     sum += int(str(root.val) + val)
-
-        # return sum
-
-        def dfs(node, current_sum):
-            if not node:
-                return 0  # Base case: No node, return 0
-
-            current_sum = current_sum * 10 + node.val  # Accumulate number
-
-            # If it's a leaf node, return the formed number
-            if not node.left and not node.right:
+            if not root:
+                return 0
+            
+            current_sum = current_sum * 10 + root.val
+            
+            if not root.left and not root.right:
                 return current_sum
 
-            # Recurse for left and right subtrees
-            return dfs(node.left, current_sum) + dfs(node.right, current_sum)
+            return helper(root.left, current_sum) + helper(root.right, current_sum)
 
-        return dfs(root, 0)
-
-
+        return helper(root, 0)
             
