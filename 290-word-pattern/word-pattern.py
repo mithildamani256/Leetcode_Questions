39ec2c2aedb992bbe9bb71fb3491f0254be600e2
mithildamani256 <63705029+mithildamani256@users.noreply.github.com>
2025-02-25 19:04:16 -0500

    Time: 0 ms (100.00%) | Memory: 12.5 MB (13.35%) - LeetSync

--- 290-word-pattern/word-pattern.py
+++ 290-word-pattern/word-pattern.py
@@ -8,25 +8,32 @@ class Solution(object):
         s = s.split()
 
         if len(pattern) != len(s):
-            return False
-        
-        values = {}
+            return False 
+
+            
+        hashmap = {}
 
         for i in range(len(pattern)):
-            if pattern[i] in values:
-                if values[pattern[i]] != s[i]:
-                    return False
-            else:
-                values[pattern[i]] = s[i]
+            if pattern[i] in hashmap:
+                value = hashmap[pattern[i]]
 
-        values = {}
+                if value != s[i]:
+                    return False
+
+            else:
+                hashmap[pattern[i]] = s[i]    
+
+
+        hashmap = {}
 
         for i in range(len(s)):
-            if s[i] in values:
-                if values[s[i]] != pattern[i]:
-                    return False
-            else:
-                values[s[i]] = pattern[i]
+            if s[i] in hashmap:
+                value = hashmap[s[i]]
 
-        
-        return True
+                if value != pattern[i]:
+                    return False
+
+            else:
+                hashmap[s[i]] = pattern[i]   
+
+        return True 
