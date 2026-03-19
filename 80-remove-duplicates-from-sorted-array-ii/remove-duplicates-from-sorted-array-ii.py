36e5f6f770c339833882060dbb4efb9618718ef4
mithildamani256 <63705029+mithildamani256@users.noreply.github.com>
2026-03-19 17:48:46 -0400

    Time: 78 ms (20.67%) | Memory: 15.3 MB (51.73%) - LeetSync

--- 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.py
+++ 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.py
@@ -4,27 +4,11 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-
-        if len(nums) <= 2:
-            return len(nums) 
-
-        hashmap = {}
-
-        for i in range(2):
-            hashmap[nums[i]] = hashmap.get(nums[i], 0) + 1
-
-        k = 2
         
+        j = 2
         for i in range(2, len(nums)):
-            value = hashmap.get(nums[i], 0)
-
-            if value <= 1:
-                nums[k] = nums[i]
-                k += 1
+            if nums[i] != nums[j-2]:
+                nums[j] = nums[i]
+                j += 1
+        return j
             
-            hashmap[nums[i]] = hashmap.get(nums[i], 0) + 1
-
-
-        return k
-
-        
