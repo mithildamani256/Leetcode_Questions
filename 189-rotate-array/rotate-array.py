350be689e223302d419653a0a5109867c64ccf04
mithildamani256 <63705029+mithildamani256@users.noreply.github.com>
2026-05-08 17:24:19 +0530

    Time: 8 ms (41.65%) | Memory: 22.4 MB (62.48%) - LeetSync

--- 189-rotate-array/rotate-array.py
+++ 189-rotate-array/rotate-array.py
@@ -8,8 +8,17 @@ class Solution(object):
 
         k = k % len(nums)
 
-        first_half_nums = nums[:len(nums) - k]
-        second_half_nums = nums[len(nums) - k : ]
+        # first_half_nums = nums[:len(nums) - k]
+        # second_half_nums = nums[len(nums) - k : ]
 
+        # nums[:] = second_half_nums + first_half_nums
 
-        nums[:] = second_half_nums + first_half_nums
+        def reverse(l,r):
+            while l < r:
+                nums[l], nums[r] = nums[r], nums[l]
+                l += 1
+                r -= 1
+            
+        reverse(0, len(nums) - 1)
+        reverse(0, k - 1)
+        reverse(k, len(nums) - 1)
