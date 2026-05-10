ee381212e6cca0ef287c40a57792e307c176bd3b
mithildamani256 <63705029+mithildamani256@users.noreply.github.com>
2026-05-11 00:55:08 +0530

    Time: 169 ms (86.68%) | Memory: 55 MB (82.29%) - LeetSync

--- 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.py
+++ 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.py
@@ -1,8 +1,8 @@
 class RandomizedSet(object):
 
     def __init__(self):
-        self.values = []
-        self.values_mapping = {}
+        self.dict = {}
+        self.set = []
 
     def insert(self, val):
         """
@@ -10,13 +10,14 @@ class RandomizedSet(object):
         :rtype: bool
         """
 
-        if val in self.values_mapping:
+        if val in self.dict:
             return False
         
-        length = len(self.values)
-        self.values.append(val)
-        self.values_mapping[val] = length
+        length_set = len(self.set)
+        self.dict[val] = length_set
 
+        self.set.append(val)
+        
         return True
 
     def remove(self, val):
@@ -25,33 +26,39 @@ class RandomizedSet(object):
         :rtype: bool
         """
 
-        if val not in self.values_mapping:
+        if val not in self.dict:
             return False
+        
+        index_val = self.dict[val]
+        last_set_element = self.set[-1]
 
-        val_index = self.values_mapping[val]
-        last_value = self.values[-1]
+        self.set[index_val] = last_set_element
+        self.set.pop()
 
-        self.values[val_index] = last_value
-        self.values.pop()
-
-        self.values_mapping[last_value] = val_index
-        del self.values_mapping[val]
+        self.dict[last_set_element] = index_val
+        del self.dict[val]
 
         return True
+        
+#  list = [3,4,1,2]
+#  dict = {3:0, 4:1, 2:3, 1:2}
+
+# 5 so for that 
+#  step 1 -> find index of 5 in dict (2), the last element in list (1)
+#  step 2 -> at index 2, put 1. and then delete last index from list and update 1 in dict to habve2 and delete the key 5. 
+        
 
     def getRandom(self):
         """
         :rtype: int
         """
 
-        return random.choice(self.values)
-    
+        return random.choice(self.set)
+        
+
 
 # Your RandomizedSet object will be instantiated and called as such:
 # obj = RandomizedSet()
 # param_1 = obj.insert(val)
 # param_2 = obj.remove(val)
 # param_3 = obj.getRandom()
-
-
-# [1,3] {1:0, 3:1} ,,,,,, remove(2), val_index = 1, last_index = 2
