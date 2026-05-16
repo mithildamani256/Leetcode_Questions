096b072edd52bd9343a80906fb5a6a088715e967
mithildamani256 <63705029+mithildamani256@users.noreply.github.com>
2026-05-16 11:13:36 +0530

    Time: 0 ms (100.00%) | Memory: 12.9 MB (5.15%) - LeetSync

--- 392-is-subsequence/is-subsequence.py
+++ 392-is-subsequence/is-subsequence.py
@@ -6,17 +6,27 @@ class Solution(object):
         :rtype: bool
         """
 
-        if s == "":
+        if not s:
             return True
 
         i = 0
-
-        for val in t:
-            if s[i] == val:
+        for j in range(len(t)):
+            if s[i] == t[j]:
                 i += 1
             
-            if i == len(s):
+            if i >= len(s):
                 return True
-
+        
         return False
+
+# Two pointer approach 
+# i = 0
+#  for j in range(len(t)):
+#       s[i] == t[j]:
+            # i += 1
+        # if i == len(s):
+        #     return Trye
+
+    # return False
+
         
