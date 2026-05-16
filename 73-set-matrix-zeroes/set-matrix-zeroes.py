f8a03a7215c2fa78effe84570b277d40a29a448b
mithildamani256 <63705029+mithildamani256@users.noreply.github.com>
2026-05-16 19:03:23 +0530

    Time: 12 ms (52.25%) | Memory: 13.1 MB (99.81%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/set-matrix-zeroes.py
@@ -4,34 +4,54 @@ class Solution(object):
         :type matrix: List[List[int]]
         :rtype: None Do not return anything, modify matrix in-place instead.
         """
+
+        zero_row_value = False
+        zero_col_value = False
+
+        for i in range(len(matrix)):
+            if matrix[i][0] == 0:
+                zero_col_value = True
+
+                break
+
+        for i in range(len(matrix[0])):
+            if matrix[0][i] == 0:
+                zero_row_value = True
+
+                break
         
-class Solution(object):
-    def setZeroes(self, matrix):
-        """
-        :type matrix: List[List[int]]
-        :rtype: None. Modify matrix in-place.
-        """
-        if not matrix:
-            return
-
-        rows, cols = len(matrix), len(matrix[0])
-        visit = set()
-
-        # Step 1: Store the positions of all zeroes
-        for i in range(rows):
-            for j in range(cols):
+        for i in range(1, len(matrix)):
+            for j in range(1, len(matrix[0])):
                 if matrix[i][j] == 0:
-                    visit.add((i, j))
+                    matrix[i][0] = 0
+                    matrix[0][j] = 0
 
-        # Step 2: Set entire row and column to zero
-        for i, j in visit:
-            for h in range(rows):
-                matrix[h][j] = 0  # Set column to zero
-            for h in range(cols):
-                matrix[i][h] = 0  # Set row to zero
- 
+        for i in range(1, len(matrix)):
+            for j in range(1, len(matrix[0])):
+                if matrix[i][0] == 0 or matrix[0][j] == 0:
+                    matrix[i][j] = 0
 
-                         
+        if zero_col_value:
+            for i in range(len(matrix)):
+                matrix[i][0] = 0
+        if zero_row_value:
+            for j in range(len(matrix[0])):
+                matrix[0][j] = 0
+
+        return
+        
 
 
 
+
+#  rows = [false, false, false, ....]
+#  cols = [false, false, false, false]
+
+#  go ovr the matrix:
+#    at each step, if char is 1, just update rows[i] and cols[j] to be true
+
+#  for row in rows:
+#       matrix[row] = [0 for _ in range(cols)]
+#  for col in cols:
+#       for r in range(len(rows)):
+#           matrix[r][col] = 0
