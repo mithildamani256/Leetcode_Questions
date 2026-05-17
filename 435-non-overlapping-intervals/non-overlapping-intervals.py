6ace4dacddbaa18e5906c728dd60add15bf5fd52
mithildamani256 <63705029+mithildamani256@users.noreply.github.com>
2026-05-17 19:06:54 +0530

    Time: 192 ms (22.45%) | Memory: 45.2 MB (27.34%) - LeetSync

--- 435-non-overlapping-intervals/non-overlapping-intervals.py
+++ 435-non-overlapping-intervals/non-overlapping-intervals.py
@@ -5,19 +5,29 @@ class Solution(object):
         :rtype: int
         """
 
-        intervals.sort(key = lambda x: x[0])
-
-        current = intervals[0]
+        intervals.sort(key = lambda x : x[0])
+        last_end = intervals[0][1]
         overlapping = 0
 
-        for interval in intervals[1:]:
-            start, end = interval
-
-            if start < current[1]:
+        for i in range(1, len(intervals)):
+            if last_end > intervals[i][0]:
+                last_end = min(intervals[i][1], last_end)
                 overlapping += 1
-                if end < current[1]:
-                    current = interval
+            
             else:
-                current = interval
+                last_end = intervals[i][1]
 
         return overlapping
+        
+# sort by start date. then go over the intervals one at a time. 
+#  for each interval if u think it is overlapping, keep the interval that ends first. 
+
+#  [[1,2],[2,3],[3,4],[1,3]]
+
+# => [[1,2],[1,3], [2,3],[3,4]]
+
+#  last_end = 2
+# [1,3] => last_end = 2
+# [2,3] => last_end = 3
+# [3,4] => overlapping not nupdated 
+# return 1
