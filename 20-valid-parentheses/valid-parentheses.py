49a474a155b36aeeaf73bb3566387f4977ae2567
mithildamani256 <63705029+mithildamani256@users.noreply.github.com>
2026-05-18 15:26:00 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (70.08%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -1,21 +1,26 @@
-from collections import deque
-
 class Solution(object):
     def isValid(self, s):
         """
         :type s: str
         :rtype: bool
         """
-        
-        stack = deque()
-        mapping = { ")" : "(" , "}" : "{", "]" : "["}
 
-        for char in s:
-            if char in mapping:
-                top_element = stack.pop() if stack else '#'
-                if mapping[char] != top_element:
-                    return False
+        brackets = {"}" : "{", ")" : "(", "]": "["}
+
+        stack = []
+
+        for value in s:
+            if value not in brackets:
+                stack.append(value)
+
             else:
-                stack.append(char)
-
-        return not stack
+                if not stack:
+                    return False 
+                    
+                opener = stack.pop()
+                if opener != brackets[value]:
+                    return False
+        
+        if not stack:
+            return True
+        return False
