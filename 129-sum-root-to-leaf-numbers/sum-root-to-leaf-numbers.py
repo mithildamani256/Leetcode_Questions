904b56735709121882e8dda903d5835316faff0d
mithildamani256 <63705029+mithildamani256@users.noreply.github.com>
2026-05-23 17:54:14 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (36.21%) - LeetSync

--- 129-sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.py
+++ 129-sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.py
@@ -1,28 +1,47 @@
 # Definition for a binary tree node.
-class TreeNode(object):
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution(object):
     def sumNumbers(self, root):
         """
         :type root: Optional[TreeNode]
         :rtype: int
-
         """
 
-        def helper(root, current_sum):
-
+        def helper(root, current_value):
             if not root:
                 return 0
-            
-            current_sum = current_sum * 10 + root.val
-            
-            if not root.left and not root.right:
-                return current_sum
 
-            return helper(root.left, current_sum) + helper(root.right, current_sum)
+            new_value = current_value * 10 + root.val
+
+            if not root.left and not root.right:
+                return new_value
+            
+            return helper(root.left, new_value) + helper(root.right, new_value)
+
+        if not root:
+            return 0
 
         return helper(root, 0)
-            
+
+
+#           4
+#          / \
+#         5.  7
+#        / \.  \
+       #1. 2.   3
+
+# current_value = 4
+
+# helper(root.left, 4) + helper(root.right,4)
+
+# if not root:
+#       return 0
+# new_value = current_value * 10 + root.val
+# if not root.left and not root.right:
+#           return new_value
+
+# helper(root.left, new_value) + helper(root.right, new_value)
