a27b817d2e3f30e0df05e9b64620463331651560
mithildamani256 <63705029+mithildamani256@users.noreply.github.com>
2026-06-14 17:09:33 -0400

    Time: 215 ms (49.63%) | Memory: 13.8 MB (4.61%) - LeetSync

---
+++ 621-task-scheduler/task-scheduler.py
@@ -0,0 +1,54 @@
+class Solution(object):
+    def leastInterval(self, tasks, n):
+        """
+        :type tasks: List[str]
+        :type n: int
+        :rtype: int
+        """
+        task_dict = {}
+        heap = []
+        queue = deque()
+
+        for task in tasks:
+            task_dict[task] = task_dict.get(task, 0) + 1
+
+        for freq in task_dict.values():
+            heapq.heappush(heap, -freq)
+
+        time = 0
+
+        while heap or queue:
+            if not heap:
+                time += 1
+
+                if queue[0][1] <= time:
+                    freq, _ = queue.popleft()
+                    heapq.heappush(heap, freq)
+                    
+                continue
+
+            current_freq = heapq.heappop(heap)
+            current_freq += 1
+            time += 1
+
+            if queue and queue[0][1] <= time:
+                freq, _ = queue.popleft()
+                heapq.heappush(heap, freq)
+
+            if current_freq != 0:
+                queue.append((current_freq, time + n))
+
+        return time
+
+
+# use a max heap to determine which is the most frequent currently but also need to keep track of what cant be done right now
+
+# tasks = ["A","C","A","B","D","B"], n = 1
+
+# heap => {-2,-2,-1,-1}, queue = []
+
+# -2 => -1
+# time = 1
+
+# heap => {-2,-1,-1}, queue = [(-1, 2)]
+# heap => {-1,-1, -1}, queue = [(-1,3)]
