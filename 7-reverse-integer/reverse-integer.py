0078bb17a4639d3cdb9ce2d23d756ce080d85a4b
mithildamani256 <63705029+mithildamani256@users.noreply.github.com>
2026-06-15 19:14:50 -0400

    Time: 13 ms (82.21%) | Memory: 12.4 MB (14.57%) - LeetSync

---
+++ 7-reverse-integer/reverse-integer.py
@@ -0,0 +1,46 @@
+import math
+
+class Solution(object):
+    def reverse(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        MIN = -2147483648  # -2^31
+        MAX = 2147483647   #  2^31 - 1
+
+        # FIX 1: Calculate boundaries using truncation (towards zero)
+        # instead of Python's floor operators.
+        MAX_DIV = int(MAX / 10)       #  214748364
+
+        MAX_MOD = MAX % 10            #  7
+        
+        MIN_DIV = -(abs(MIN) // 10)      # -214748364
+
+        MIN_MOD = int(math.fmod(MIN, 10))   # -8
+        
+        res = 0
+        while x:
+            digit = int(math.fmod(x, 10))
+            print(digit)
+
+            if x < 0:
+                x = -int(abs(x) // 10)  
+            else:
+                x = int(x / 10)
+                
+            print(x)              
+            
+            # Positive overflow check
+            if (res > MAX_DIV or 
+                (res == MAX_DIV and digit >= MAX_MOD)):
+                return 0
+                
+            # FIX 2: Used the corrected MIN truncation boundaries here
+            if (res < MIN_DIV or 
+                (res == MIN_DIV and digit <= MIN_MOD)):
+                return 0
+                
+            res = (res * 10) + digit
+
+        return res
