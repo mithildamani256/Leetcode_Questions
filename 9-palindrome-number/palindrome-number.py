c3892e78f44987ad3b66dfe53002d94c1e5cdbf5
mithildamani256 <63705029+mithildamani256@users.noreply.github.com>
2026-06-15 21:00:21 -0400

    Time: 12 ms (49.06%) | Memory: 12.5 MB (18.36%) - LeetSync

--- 9-palindrome-number/palindrome-number.py
+++ 9-palindrome-number/palindrome-number.py
@@ -4,10 +4,27 @@ class Solution(object):
         :type x: int
         :rtype: bool
         """
-        x = str(x)
-        length = len(x)
-        for i in range(len(x) // 2):
-            if x[i] != x[length - i - 1]:
+
+        if x < 0:
+            return False
+
+        div = 1
+
+        while x >= 10 * div:
+            div = div * 10
+
+        while x:
+            right = x % 10
+            left = x // div
+
+            if right != left:
                 return False
-        
+            
+            x = (x % div) // 10
+            div = div // 100
+
         return True
+
+
+
+        
