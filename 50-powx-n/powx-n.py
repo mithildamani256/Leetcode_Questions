06c4973f27a62603becf91548af9c5b5cc49ed58
mithildamani256 <63705029+mithildamani256@users.noreply.github.com>
2026-06-16 13:31:23 -0400

    Time: 0 ms (100.00%) | Memory: 12.4 MB (63.80%) - LeetSync

--- 50-powx-n/powx-n.py
+++ 50-powx-n/powx-n.py
@@ -1,17 +1,11 @@
 class Solution(object):
     def myPow(self, x, n):
-        if n == 0:
-            return 1
+        """
+        :type x: float
+        :type n: int
+        :rtype: float
+        """
+
+        return pow(x, n)
+
         
-        if n < 0:
-            x = 1 / x
-            n = -n
-        
-        result = 1
-        while n > 0:
-            if n % 2 == 1:
-                result *= x
-            x *= x
-            n //= 2
-        
-        return result
