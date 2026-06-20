da35bf4824bbd2f90f6c6db1908a6f715c735415
mithildamani256 <63705029+mithildamani256@users.noreply.github.com>
2026-06-20 13:57:32 -0400

    Time: 19 ms (87.23%) | Memory: 13.3 MB (30.14%) - LeetSync

--- 55-jump-game/jump-game.py
+++ 55-jump-game/jump-game.py
@@ -8,15 +8,14 @@ class Solution(object):
         if len(nums) == 1:
             return True
 
-        target = len(nums) - 1
+        goal = len(nums) - 1
 
         for i in range(len(nums) - 2, -1, -1):
-            if i + nums[i] >= target:
-                target = i
-            if target == 0:
+            if i + nums[i] >= goal:
+                goal = i
+
+            if goal == 0:
                 return True
-
+        
         return False
-            
-
         
