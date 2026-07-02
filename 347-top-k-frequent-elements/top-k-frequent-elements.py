b154e98946a1dadad35d67894326959b93cbe5f2
mithildamani256 <63705029+mithildamani256@users.noreply.github.com>
2026-07-02 13:14:06 -0400

    Time: 19 ms (17.91%) | Memory: 20.1 MB (8.08%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -7,19 +7,45 @@ class Solution(object):
         """
 
         hashmap = {}
-        heap = []
 
         for value in nums:
             hashmap[value] = hashmap.get(value, 0) + 1
         
-        for value in hashmap:
-            heapq.heappush(heap, (- hashmap[value], value))
+        buckets = [[] for _ in range(len(nums) + 1)]
 
-        result = []
+        for value,freq in hashmap.items():
+            buckets[freq].append(value)
+        
+        res = []
 
-        for _ in range(k):
-            count, value = heapq.heappop(heap)
-            result.append(value)
+        count = k
 
-        return result
+        for i in range(len(nums), -1, -1):
+            for value in buckets[i]:
+                res.append(value)
+
+                if len(res) == k:
+                    return res
+
+
+        # hashmap = {}
+
+        # for value in nums:
+        #     hashmap[value] = hashmap.get(value, 0) + 1
+
+        # heap = []
+
+        # for value in hashmap:
+        #     heap.append((-hashmap[value], value))
+
+        # heapq.heapify(heap)
+
+        # result = []
+
+        # for _ in range(k):
+        #     count, value = heapq.heappop(heap)
+
+        #     result.append(value)
+
+        # return result
         
