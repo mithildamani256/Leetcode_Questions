3c5a454221d5635a2e7312f57756eb590f53d198
mithildamani256 <63705029+mithildamani256@users.noreply.github.com>
2026-07-06 15:02:53 -0400

    Time: 239 ms (19.73%) | Memory: 29.6 MB (5.11%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.py
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.py
@@ -5,25 +5,51 @@ class Solution(object):
         :rtype: int
         """
 
-        stack = []
-        max_area = 0
+        if not heights:
+            return 0
 
-        for i in range(len(heights)):
-            current = heights[i] 
+        max_area = 0
+        
+        stack = []
+
+        stack.append((heights[0],0))
+
+        for i in range(1, len(heights)):
+            current_height = heights[i]
             until = i
 
-            while stack and (current < stack[-1][1]):
-                index, prev = stack.pop()
-                prev_area = prev * (i - index)
-                max_area = max(max_area, prev_area)
-
+            while stack and  stack[-1][0] > current_height:
+                prev, index = stack.pop()
+                max_area = max(max_area, (i - index) * prev)
                 until = index
-        
-            stack.append((until, current))
+            
+            stack.append((current_height, until))
+
 
         while stack:
-            i, h = stack.pop()
+            height, start = stack.pop()
 
-            max_area = max(max_area, h * (len(heights) - i))
-        
+            max_area = max(max_area, (len(heights) - start) * height)
+
         return max_area
+        
+
+
+
+
+# [2,1,5,6,2,3]
+
+# =>
+
+# stack - [(2,0)] (monotonically increasing stack)
+# when you see 1, you pop form the stack to get 2. 
+# the area u can get from 2 is (index of 1 - whatever 2 had on stack) * 2
+
+# stack = [(1,0)]
+# stack = [1,5,6]
+# 2 
+# pop 6. calculate the area -> ()
+# pop 5, calculate the area
+# but now when adding 2, u need to add (2,2)
+# u need to add the index of where you can start to take area including this element
+        
