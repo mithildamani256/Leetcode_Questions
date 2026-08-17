503c8e5b4965f33364fc98597a472adc6f287cca
mithildamani256 <63705029+mithildamani256@users.noreply.github.com>
2026-08-17 12:53:10 +0530

    Time: 3 ms (66.41%) | Memory: 13.7 MB (86.69%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
@@ -5,20 +5,16 @@ class Solution(object):
         :rtype: int
         """
 
-        if prices == []:
-            return 0
-
-        buy_price = prices[0]
-        profit = 0
+        total = 0
+        buy = prices[0]
 
         for i in range(1, len(prices)):
-            if prices[i] < buy_price:
-                buy_price = prices[i]
-            else:
-                current_profit = prices[i] - buy_price
-                profit += current_profit
+            price = prices[i]
 
-                buy_price = prices[i]
-        
-        return profit
-        
+            if price <= buy:
+                buy = price
+            else:
+                total += price - buy
+                buy = price
+
+        return total 
