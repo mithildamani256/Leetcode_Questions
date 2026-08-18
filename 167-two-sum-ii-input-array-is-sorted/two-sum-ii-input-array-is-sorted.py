17e0028db22cde93253a94dd4d00b57d1b9d228a
mithildamani256 <63705029+mithildamani256@users.noreply.github.com>
2026-08-18 11:54:33 +0530

    Time: 8 ms (28.47%) | Memory: 12.8 MB (45.92%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
@@ -6,33 +6,19 @@ class Solution(object):
         :rtype: List[int]
         """
 
-        # l = 0
-        # h = len(numbers) - 1
-
-        # while(l < h):
-        #     total = numbers[l] + numbers[h]
-        #     if( total == target):
-        #         return [l + 1, h + 1]
-        #     elif ( total > target):
-        #         h -= 1
-        #     else:
-        #         l += 1
-        
-        L = []
-
         i = 0
         j = len(numbers) - 1
 
-        while i < j:
+        while True:
             current_sum = numbers[i] + numbers[j]
-
-            if current_sum > target:
+            if current_sum == target:
+                return [i + 1,j + 1]
+            
+            elif current_sum > target:
                 j -= 1
-            elif current_sum < target:
-                i += 1
+            
             else:
-                L.append(i + 1)
-                L.append(j+ 1)
-                break
+                i += 1
         
-        return L
+        return
+        
