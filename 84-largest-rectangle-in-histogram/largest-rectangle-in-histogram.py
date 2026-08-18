58ca3a2a407d5dd1e85c509d1bf445f4a16cfcf5
mithildamani256 <63705029+mithildamani256@users.noreply.github.com>
2026-08-18 14:50:18 +0530

    Time: 199 ms (86.67%) | Memory: 28.4 MB (10.96%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.py
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.py
@@ -5,51 +5,22 @@ class Solution(object):
         :rtype: int
         """
 
-        if not heights:
-            return 0
-
-        max_area = 0
-        
         stack = []
+        max_area = 0
 
-        stack.append((heights[0],0))
-
-        for i in range(1, len(heights)):
-            current_height = heights[i]
+        for i, height in enumerate(heights):
             until = i
+            while stack and stack[-1][1] > height:
+                index, last_height = stack.pop()
+                max_area = max(max_area, (i - index) * last_height)
 
-            while stack and  stack[-1][0] > current_height:
-                prev, index = stack.pop()
-                max_area = max(max_area, (i - index) * prev)
                 until = index
             
-            stack.append((current_height, until))
+            stack.append((until, height))
 
-
-        while stack:
-            height, start = stack.pop()
-
-            max_area = max(max_area, (len(heights) - start) * height)
+        for index, height in stack:
+            area = (len(heights) - index) * height
+            max_area = max(area, max_area)
 
         return max_area
-        
-
-
-
-
-# [2,1,5,6,2,3]
-
-# =>
-
-# stack - [(2,0)] (monotonically increasing stack)
-# when you see 1, you pop form the stack to get 2. 
-# the area u can get from 2 is (index of 1 - whatever 2 had on stack) * 2
-
-# stack = [(1,0)]
-# stack = [1,5,6]
-# 2 
-# pop 6. calculate the area -> ()
-# pop 5, calculate the area
-# but now when adding 2, u need to add (2,2)
-# u need to add the index of where you can start to take area including this element
         
